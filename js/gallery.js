382cb9e555d4322c70ac664adf89d872b5aa2b41
Aleksey Yesin <esinru@gmail.com>
2021-02-28 21:17:50 +0200

    Gallery rendering

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,25 @@
-import galleryData from "./data/gallery-items.js";
+import galleryDataArr from './data/gallery-items.js';
+
+const galleryListRef = document.querySelector('.js-gallery');
+
+renderGalleryDataAtEnd(galleryDataArr);
+
+function generateGalleryMarkup(galleryDataArr) {
+  return galleryDataArr.reduce(
+    (markupAcc, { preview, original, description }) =>
+      markupAcc +
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </li>`,
+    '',
+  );
+}
+
+function renderGalleryDataAtEnd(galleryDataArr) {
+  galleryListRef.insertAdjacentHTML(
+    'beforeend',
+    generateGalleryMarkup(galleryDataArr),
+  );
+}
