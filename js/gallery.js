1fe4a5ec5eb4f544cc43583718c6671d663d04ba
Aleksey Yesin <esinru@gmail.com>
2021-03-01 03:56:10 +0200

    Esc, mouse modal close

--- js/gallery.js
+++ js/gallery.js
@@ -5,14 +5,20 @@ import galleryDataArr from './data/gallery-items.js';
 const galleryListRef = document.querySelector('.js-gallery');
 const lightboxRef = document.querySelector('.js-lightbox');
 const lightboxCloseBtnRef = lightboxRef.querySelector(
-  '[data-action="close-lightbox"]',
+  'button[data-action="close-lightbox"]',
 );
 const lightboxImgRef = lightboxRef.querySelector('img');
+const lightboxOverlayRef = lightboxRef.querySelector(
+  'div[data-action="close-lightbox"]',
+);
 
 renderGalleryToEnd(galleryDataArr);
 
+let currentGalleryImgRef = null;
+
 galleryListRef.addEventListener('click', onGalleryListClick);
 lightboxCloseBtnRef.addEventListener('click', closeLightbox);
+lightboxOverlayRef.addEventListener('click', closeLightbox);
 
 // ==================== 📌 GALLERY FUNCTIONS ====================
 
@@ -41,14 +47,29 @@ function onGalleryListClick(e) {
 
   if (!e.target.classList.contains('gallery__image')) return;
 
-  openLightbox(e.target.dataset.source, e.target.alt);
+  openLightbox(e.target);
+}
+
+function getPrevGalleryImg() {
+  return currentGalleryImgRef
+    .closest('.gallery__item')
+    .previousElementSibling?.querySelector('.gallery__image');
+}
+
+function getNextGalleryImg() {
+  return currentGalleryImgRef
+    .closest('.gallery__item')
+    .nextElementSibling?.querySelector('.gallery__image');
 }
 
 // ==================== 📌 LIGHTBOX FUNCTIONS ====================
 
-function openLightbox(imgSrc, imgAlt) {
-  lightboxImgRef.src = imgSrc;
-  lightboxImgRef.alt = imgAlt;
+function openLightbox(imgRef) {
+  currentGalleryImgRef = imgRef;
+  lightboxImgRef.src = imgRef.dataset.source;
+  lightboxImgRef.alt = imgRef.alt;
+
+  window.addEventListener('keydown', onLightboxKeyDown);
 
   lightboxRef.classList.add('is-open');
 }
@@ -56,6 +77,29 @@ function openLightbox(imgSrc, imgAlt) {
 function closeLightbox() {
   lightboxRef.classList.remove('is-open');
 
+  window.removeEventListener('keydown', onLightboxKeyDown);
+
   lightboxImgRef.src = '';
   lightboxImgRef.alt = '';
+  currentGalleryImgRef = null;
+}
+
+function onLightboxKeyDown(e) {
+  const ESC_KEY_CODE = 'Escape';
+  const ARROW_LEFT_KEY_CODE = 'ArrowLeft';
+  const ARROW_RIGHT_KEY_CODE = 'ArrowRight';
+
+  // if (e.code === ESC_KEY_CODE) closeLightbox();
+
+  switch (e.code) {
+    case ESC_KEY_CODE:
+      closeLightbox();
+      break;
+    case ARROW_LEFT_KEY_CODE:
+      console.log(getPrevGalleryImg());
+      break;
+    case ARROW_RIGHT_KEY_CODE:
+      // console.log(currentGalleryImgRef.nextElementSibling);
+      break;
+  }
 }
